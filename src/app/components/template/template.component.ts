3562f0bfad8c4fefdaf5a456ba9c04dac11c7b2c
Samir Mejia <samir04m@gmail.com>
2019-11-03 17:05:04 -0500

    Validacion de campos en proceso

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-template',
-  templateUrl: './template.component.html',
-  styles: []
+   selector: 'app-template',
+   templateUrl: './template.component.html',
+   styles: [`
+      .ng-invalid.ng-touched:not(form) {
+         border: 1px solid red;
+      }
+   `]
 })
 export class TemplateComponent implements OnInit {
+
+   public usuario:Object = {
+      nombre:null,
+      apellido:null,
+      email: null
+   }
 
   constructor() { }
 
   ngOnInit() {
   }
+
+   guardar(forma:NgForm){
+     console.log("forma",forma);
+     console.log("value",forma.value);
+   }
 
 }

25c045b99fa7660dfedfcf2e667cd91709539f04
Samir Mejia <samir04m@gmail.com>
2019-11-04 15:59:24 -0500

    Llenar campos desde data.component

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -23,16 +23,16 @@ export class DataComponent implements OnInit {
       this.forma = new FormGroup({
 
          'nombreCompleto': new FormGroup ({
-            'nombre': new FormControl('', [
+            'nombre': new FormControl(this.usuario.nombreCompleto.nombre, [
                                              Validators.required,
                                              Validators.minLength(3)
                                           ]),
-            'apellido': new FormControl('', [
+            'apellido': new FormControl(this.usuario.nombreCompleto.apellido, [
                                              Validators.required,
                                              Validators.minLength(3)
                                           ])
          }),
-         'email': new FormControl('', [
+         'email': new FormControl(this.usuario.email , [
                                        Validators.required,
                                        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
                                     ])

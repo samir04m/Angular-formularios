a0f08c054086b43a7ba022955fa13a0c7d126c8e
Samir Mejia <samir04m@gmail.com>
2019-11-04 15:56:30 -0500

    FormGroup, objetos dentro de objetos

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,16 +10,28 @@ export class DataComponent implements OnInit {
 
    forma:FormGroup;
 
+   usuario:Object = {
+
+      nombreCompleto: {
+         nombre: "Samir",
+         apellido: "Mejia"
+      },
+      email: "samir@gmail.com"
+   };
+
    constructor() {
       this.forma = new FormGroup({
-         'nombre': new FormControl('', [
-                                          Validators.required,
-                                          Validators.minLength(3)
-                                       ]),
-         'apellido': new FormControl('', [
-                                          Validators.required,
-                                          Validators.minLength(3)
-                                       ]),
+
+         'nombreCompleto': new FormGroup ({
+            'nombre': new FormControl('', [
+                                             Validators.required,
+                                             Validators.minLength(3)
+                                          ]),
+            'apellido': new FormControl('', [
+                                             Validators.required,
+                                             Validators.minLength(3)
+                                          ])
+         }),
          'email': new FormControl('', [
                                        Validators.required,
                                        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")

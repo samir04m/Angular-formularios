fd8c134daf0cba063f39e8c0e4c9a5dc420db12e
Samir Mejia <samir04m@gmail.com>
2019-11-06 15:56:55 -0500

    Validacion de passwords

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -21,29 +21,48 @@ export class DataComponent implements OnInit {
    };
 
    constructor() {
+
       this.forma = new FormGroup({
 
          'nombreCompleto': new FormGroup ({
             'nombre': new FormControl('', [
-                                             Validators.required,
-                                             Validators.minLength(3)
-                                          ]),
+               Validators.required,
+               Validators.minLength(3)
+            ]),
             'apellido': new FormControl('', [
-                                             Validators.required,
-                                             Validators.minLength(3)
-                                          ])
+               Validators.required,
+               Validators.minLength(3)
+            ])
          }),
          'email': new FormControl('', [
-                                       Validators.required,
-                                       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
-                                    ]),
+            Validators.required,
+            Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+         ]),
          'pasatiempos': new FormArray ([
             new FormControl('Correr', Validators.required)
-         ])
+         ]),
+         'password1': new FormControl('', Validators.required),
+         'password2': new FormControl()
       });
 
       // this.forma.setValue(this.usuario);
 
+      this.forma.controls['password2'].setValidators([
+         Validators.required,
+         this.noIgual.bind(this.forma)
+      ]);
+   }
+
+   noIgual(control: FormControl): { [s:string]:boolean } {
+
+      let forma:any = this;
+
+      if (control.value !== forma.controls['password1'].value){
+         return {
+            noiguales:true
+         }
+      }
+      return null;
    }
 
    guardarCambios(){
@@ -58,6 +77,8 @@ export class DataComponent implements OnInit {
          email:""
       });
    }
+
+
 
    agregarPasatiempo(){
       (<FormArray>this.forma.controls['pasatiempos']).push(
